e774b0e31eaf7cbe8d3c6f748863acb20fb30166
淘杰 <taojie.hjp@taobao.com>
2014-08-19 18:57:45 +0800

    build

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,40 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    kmc = require('gulp-kmc'),
+    kclean = require('gulp-kclean');
+
+
+var src = './lib',
+    build = './build',
+    buildFile = build+'/xtemplate-combo.js';
+
+kmc.config({
+    packages:{
+       "xtemplate":{
+           combine:true,
+           base: src
+        }
+    }
+});
 
 gulp.task('build', function () {
+    gulp.src(src+'/**/*.js')
+        .pipe(kmc.convert())
+        .pipe(kmc.combo({
+             files:[{
+                 src: src+'/xtemplate.js',
+                 dest: buildFile
+             }]
+        }))
+        .pipe(kclean({
+            files:[{
+                  src:buildFile,
+                  wrap:{
+                    start:"var xtemplate = (function(){",
+                    end:'\nreturn xtemplate;\n})()'
+                  }
+            }]
+         }))
+        .pipe(gulp.dest(build));
 });
 
 gulp.task('default', ['server'], function () {
