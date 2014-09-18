d90e4198ef9c5a7b7321e831bb246c742100feaa
yiminghe <yiminghe@gmail.com>
2014-09-18 14:50:39 +0800

    switch to koa

--- server.js
+++ server.js
@@ -1,37 +1,44 @@
 var gutil = require('gulp-util');
-var express = require('express');
+var koa = require('koa');
+var koaBody = require('koa-body');
 var path = require('path');
-var jscoverHandler = require('node-jscover-handler');
-var jscoverCoveralls = require('node-jscover-coveralls');
-var serveStatic = require('serve-static');
-var serveIndex = require('serve-index');
+var jscoverHandler = require('node-jscover-handler/lib/koa');
+var jscoverCoveralls = require('node-jscover-coveralls/lib/koa');
+var serve = require('koa-static');
+var serveIndex = require('koa-serve-index');
 var fs = require('fs');
-var app = express();
+var app = koa();
+var mount = require('koa-mount');
 var cwd = process.cwd();
-var bodyParser = require('body-parser');
 
-function modularize(req, res, next) {
+function *modularize(next) {
+    var req = this.request;
+    var res = this.response;
+    if (path.extname(this.path) !== '.js') {
+        yield *next;
+        return;
+    }
     var filePath = path.resolve(cwd, req.originalUrl.substring(1)).replace(/-coverage\.js/, '.js');
     var stats = fs.statSync(filePath);
     if (!stats.isFile()) {
-        next();
+        yield *next;
         return;
     }
     var code = fs.readFileSync(filePath, 'utf-8');
     code = 'modulex.add(function(require,exports,module){' + code + '});';
     if (req.path.indexOf('-coverage.js') !== -1) {
         req.nodeJsCoverCode = code;
-        next();
+        yield *next;
         return;
     }
     res.set('content-type', 'application/javascript;charset=utf-8');
-    res.end(code);
+    this.body = code;
 }
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use('/lib/', modularize);
-app.use('/tests/browser/specs/', modularize);
+app.use(koaBody());
+app.use(mount('/lib/', modularize));
+app.use(mount('/tests/browser/specs/', modularize));
 
 app.use(jscoverCoveralls());
 app.use(jscoverHandler());
@@ -39,7 +46,9 @@ app.use(serveIndex(cwd, {
     hidden: true,
     view: 'details'
 }));
-app.use(serveStatic(cwd));
+app.use(serve(cwd, {
+    hidden: true
+}));
 var port = process.env.PORT || 8002;
 app.listen(port);
 gutil.log('server start at ' + port);
